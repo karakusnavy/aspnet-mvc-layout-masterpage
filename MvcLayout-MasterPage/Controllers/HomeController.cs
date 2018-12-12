84e4bb6659a168391f2cbccd8b066c1d32e00b4e
Samed Karakus <karakusnavy@gmail.com>
2018-12-12 16:41:55 +0300

    updated

---
+++ MvcLayout-MasterPage/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLayout_MasterPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("about")]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [Route("contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcLayout-MasterPage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcLayout_MasterPage.MvcApplication" Language="C#" %>
